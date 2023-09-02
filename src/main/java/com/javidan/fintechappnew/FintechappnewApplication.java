104c6d5f1ffec1277fa1682ce06d0b1d2161b943
JavidanH <jahajiyev@gmail.com>
2023-09-02 13:13:16 +0400

    first commit

---
+++ src/main/java/com/javidan/fintechappnew/FintechappnewApplication.java
@@ -0,0 +1,13 @@
+package com.javidan.fintechappnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechappnewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FintechappnewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javidan/fintechappnew/FintechappnewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javidan.fintechappnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechappnewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
